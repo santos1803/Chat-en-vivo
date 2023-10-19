0eda276bfa2be0b1232ab3e401cbcf74d35ddd5c
unknown <santosodontologia24@gmaol.com>
2023-10-19 12:52:08 -0400

    creando el servidor y estilos del chat

---
+++ server/index.js
@@ -0,0 +1,39 @@
+import express from "express"
+import logger from "morgan"
+
+import { Server } from "socket.io"
+import {createServer} from "node:http"
+
+const port = process.env.PORT ?? 3000
+
+const app = express()
+const server = createServer(app)
+const io = new Server(server, {
+    connectionStateRecovery: {
+        
+    }
+})
+
+
+
+io.on("connection", (socket)=>{
+    console.log("Un usuario se ha conectado")
+
+    socket.on("disconnect", () =>{
+        console.log("Un usuario se ha desconectado")
+    })
+
+    socket.on("chat message", (msg)=> {
+        io.emit("chat message", msg)
+    })
+})
+
+app.use(logger("dev"))
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + "/client/index.html")
+})
+
+server.listen(port, ()=>{
+    console.log(`Server runing on port ${port}`)
+})
