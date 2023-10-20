907938ab90a40e7484a5d82cbf5717bc92fc42c2
santos1803 <santosodontologia24@gmail.com>
2023-10-20 14:46:05 -0400

    Agregando usuarios aleatorios y recuperacion de mensajes

--- server/index.js
+++ server/index.js
@@ -1,53 +1,82 @@
-import express from "express"
-import logger from "morgan"
-import dotenv from "dotenv"
-import { createClient } from "@libsql/client"
+import express from "express";
+import logger from "morgan";
+import dotenv from "dotenv";
+import { createClient } from "@libsql/client";
 
-import { Server } from "socket.io"
-import {createServer} from "node:http"
+import { Server } from "socket.io";
+import { createServer } from "node:http";
 
-const port = process.env.PORT ?? 3000
+dotenv.config();
 
-const app = express()
-const server = createServer(app)
+const port = process.env.PORT ?? 3001;
+
+const app = express();
+const server = createServer(app);
 const io = new Server(server, {
-    connectionStateRecovery: {
-        
-    }
-})
-
+  connectionStateRecovery: {},
+});
 
 const db = createClient({
-    url:"libsql://wanted-bloodscream-santos1803.turso.io",
-    authToken: process.env.DB_TOKEN
+  url: "libsql://correct-madrox-santos1803.turso.io",
+  authToken: process.env.DB_TOKEN,
+});
+
+await db.execute(` 
+CREATE TABLE IF NOT EXISTS messages (
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+content TEXT,
+user TEXT
+
+);
+
+`);
+
+io.on("connection", async (socket) => {
+  console.log("Un usuario se ha conectado");
+
+  socket.on("disconnect", () => {
+    console.log("Un usuario se ha desconectado");
+  });
+
+  socket.on("chat message", async (msg) => {
+    let result
+    const username = socket.handshake.auth.username ?? "anonymous"
+    try {
+      
+      result = await db.execute({
+        sql: "INSERT INTO messages (content, user) VALUES (:msg, :username)",
+        args: { msg, username }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+
+    io.emit("chat message", msg, result.lastInsertRowid.toString(), username)
+  })
+
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: "SELECT id, content, user FROM messages WHERE id > ?",
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row => {
+        socket.emit("chat message", row.content, row.id.toString(), row.user)
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
 })
 
-await db.execute (` 
-CREATE TABLE IF NOT EXISTS users (
-
-
-
-`)
-
-
-io.on("connection", (socket)=>{
-    console.log("Un usuario se ha conectado")
-
-    socket.on("disconnect", () =>{
-        console.log("Un usuario se ha desconectado")
-    })
-
-    socket.on("chat message", (msg)=> {
-        io.emit("chat message", msg)
-    })
-})
-
-app.use(logger("dev"))
+app.use(logger("dev"));
 
 app.get("/", (req, res) => {
-    res.sendFile(process.cwd() + "/client/index.html")
-})
+  res.sendFile(process.cwd() + "/client/index.html");
+});
 
-server.listen(port, ()=>{
-    console.log(`Server runing on port ${port}`)
-})
+server.listen(port, () => {
+  console.log(`Server runing on port ${port}`);
+});
